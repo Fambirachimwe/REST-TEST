79beb5b8ab49cf971c766dd9e1d7c71515ba14aa
Fambirachimwe <t.svaku@gmail.com>
2019-07-30 16:44:54 +0200

    added the database

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,43 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product');
+const mongoose = require('mongoose');
 
 
 router.get('/', (req, res, next) =>{
-    res.status(200).json({
-        "message": "handling get request"
+    Product.find()
+    .exec()
+    .then(doc =>{
+        res.status(200).json(doc);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err})
     });
 });
 
 router.post('/', (req, res, next) =>{
-    const product = {
+    const product = new Product({
+        _id: new  mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
-    res.status(200).json({
-        "message": "handling post request",
-        createdProduct: product
     });
+    product.save().then(result =>{
+        console.log(result);
+        res.status(200).json({
+            "message": "handling post request",
+            createdProduct: product
+        });
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err})
+    });
+    
 });
 
 router.get('/:productId', (req, res, next) =>{
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'get the product with the productIs ' +  id,
+    Product.findById(id)
+    .exec()
+    .then( doc =>{
+        if(doc){
+            res.status(200).json(doc)
+        } else{
+            res.status(404).json({
+                message: 'No valid entry for provided id '
+            });
+        }
+        
     })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err})
+    });
+    
 });
 
 
 router.patch('/:productId', (req, res, next) =>{
-    res.status(200).json({
-        message: 'updated the product id'
+    const id = req.params.productId;
+    const updatesOps = {};
+    for( const ops of req.body){   /// apa ndogona kisa yangu method 
+        updatesOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, {$set: updatesOps})
+        .exec()
+        .then(result =>{
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({error: err})
+        });
     });
-});
 
 
 router.delete('/:productId', (req, res, next) =>{
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'product deleted  ' + id,
+    Product.findOneAndRemove({_id: id})
+    .exec()
+    .then(result =>{
+        console.log(result);
+        res.status(200).json({
+            message: id + ' deleted',
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({error: err})
     });
 });
 
